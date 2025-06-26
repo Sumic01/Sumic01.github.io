aa319a5aaf1b641417d629851df974ba9ae38d9e
Uros Sumic <sumicuros01@gmail.com>
2025-06-26 14:04:20 +0200

    .

--- portfolio_website/next.config.mjs
+++ portfolio_website/next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.NODE_ENV === 'production';
-const repo = 'portfolio_website';
 
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? `/${repo}` : '',
+  basePath: '',
   trailingSlash: true,
   images: {
     unoptimized: true,  // Disable Image Optimization for static export
